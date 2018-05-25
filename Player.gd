c916786594197e6798d90bae7856a311a19f84f8
Jonas Hole <Cvpp7NwQPnSkBCn>
2018-05-25 11:52:25 +0200

    You can speak! And place trees!

--- Player.gd
+++ Player.gd
@@ -8,25 +8,35 @@ var up
 var down
 var left
 var right
+var primary
 
 var canWalk = false
 
 var tileMap
+var vegMap
+var iteMap
+
+var hasAxe = false
 
 func _ready():
 	$WalkTimer.wait_time = 0.35
 	$WalkTimer.start()
 	tileMap = get_parent().get_child(0)
+	vegMap = get_parent().get_child(1)
 	pass
 
 
 func _physics_process(delta):
 	var mov = _updateInput() * 16
+	$Label.text = ""
 	if canWalk && tileMap.get_cellv(tileMap.world_to_map(position + mov)) != -1:
-		move_and_collide(mov)
-		canWalk = false
-		$WalkTimer.start()
-	pass
+		move(mov)
+	if primary && vegMap.get_cellv(vegMap.world_to_map(position)) != -1:
+		if hasAxe:
+			vegMap.set_cellv(vegMap.world_to_map(position), -1)
+		else:
+			say("WOULD BE NICE IF I COULD PICK UP THIS TREE...")
+			
 
 func _updateInput():
 	var dir = Vector2(0,0)
@@ -34,6 +44,7 @@ func _updateInput():
 	down = false
 	left = false
 	right = false
+	primary = false
 	if Input.is_action_pressed("up"):
 		dir.y -= 1
 		up = true
@@ -46,9 +57,19 @@ func _updateInput():
 	if Input.is_action_pressed("right"):
 		dir.x += 1
 		right = true
+	if Input.is_action_pressed("primary"):
+		primary = true
 	return dir
 	pass
+
+func say(text):
+	$Label.text = str(text)
 
 func _on_WalkTimer_timeout():
 	canWalk = true
 	pass
+
+func move(m):
+	move_and_collide(m)
+	canWalk = false
+	$WalkTimer.start()
